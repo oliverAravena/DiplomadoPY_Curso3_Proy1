70a63e4daa98e003861f54346ca52973ca967e7d
Oliver Aravena <oliver.aravena@gmail.com>
2023-06-04 17:14:14 -0400

    Update MiniProyecto1.py

--- MiniProyecto1.py
+++ MiniProyecto1.py
@@ -1,7 +1,5 @@
-#Importar el conector de Python con Mysql.
 import  mysql.connector as db
 
-#Con el conector importado creamos un objeto de conexion.
 mydb = db.connect(
     host = "localhost",
     user = "root",
@@ -9,18 +7,15 @@ mydb = db.connect(
     database = "InfoAeropuertos"
 )
 
-#Con la conexion creamos un objeto cursor.
-my_cursor = mydb.cursor()
+cursor = mydb.cursor()
 
-#Creamos una sentencia SQL asociado a una variable.
-sqlsentense = " SELECT * FROM InfoAeropuertos.aeropuertos ;"
 
-#Invocamos con execute sobre el objeto cursor, la sentencia SQL que aplicaremos a la base de datos.
-my_cursor.execute(sqlsentense)
+SQLCrear_Registro = 'INSERT INTO aeropuertos (id, ident, type, name, elevation_ft, municipality, iata_code, score) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)'
 
-for x in my_cursor:
-
-    registro = my_cursor.fetchone()
-
-    print (registro)
-
+registros = [ 
+    (39340, 'SHCC', 'heliport', 'Clinica Las Condes Heliport', 2461, 'Santiago', , 25),
+    (39379, 'SHMA', 'heliport', 'Clinica Santa Maria Heliport', 2028, 'Santiago', , 25),
+    (39390, 'SHPT', 'heliport', 'Portillo Heliport', 9000, 'Los Andes', , 25)
+]
+cursor.executemany(SQLCrear_Registro, registros)
+mydb.commit()
