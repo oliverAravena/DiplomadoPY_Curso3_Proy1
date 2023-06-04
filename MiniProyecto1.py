d9c46a0dfb38b0a06c40c694af2e691abe7ae845
Oliver Aravena <oliver.aravena@gmail.com>
2023-06-04 17:30:01 -0400

    Update MiniProyecto1.py

--- MiniProyecto1.py
+++ MiniProyecto1.py
@@ -10,12 +10,18 @@ mydb = db.connect(
 cursor = mydb.cursor()
 
 
-SQLCrear_Registro = 'INSERT INTO aeropuertos (id, ident, type, name, elevation_ft, municipality, iata_code, score) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)'
+# SQLCrear_Registro = 'INSERT INTO aeropuertos (id, ident, type, name, elevation_ft, municipality, iata_code, score) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)'
 
-registros = [ 
-    (39340, 'SHCC', 'heliport', 'Clinica Las Condes Heliport', 2461, 'Santiago', , 25),
-    (39379, 'SHMA', 'heliport', 'Clinica Santa Maria Heliport', 2028, 'Santiago', , 25),
-    (39390, 'SHPT', 'heliport', 'Portillo Heliport', 9000, 'Los Andes', , 25)
-]
-cursor.executemany(SQLCrear_Registro, registros)
-mydb.commit()
+# registros = [ 
+#     (39340, 'SHCC', 'heliport', 'Clinica Las Condes Heliport', 2461, 'Santiago','',25),
+#     (39379, 'SHMA', 'heliport', 'Clinica Santa Maria Heliport', 2028, 'Santiago','',25),
+#     (39390, 'SHPT', 'heliport', 'Portillo Heliport', 9000, 'Los Andes','',25)
+# ]
+# cursor.executemany(SQLCrear_Registro, registros)
+# mydb.commit()
+
+SQL_Consulta = 'SELECT name, type, municipality, elevation_ft FROM aeropuertos WHERE elevation_ft > 5000'
+cursor.execute(SQL_Consulta)
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
